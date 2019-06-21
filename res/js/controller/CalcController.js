d80c25507b778116ff2b46883bc29f80a3315dcc
Gabriel Camargo <gabriel.rodrigocam@gmail.com>
2019-06-21 15:08:15 -0300

    aula C09 - switch

--- res/js/controller/CalcController.js
+++ res/js/controller/CalcController.js
@@ -2,6 +2,7 @@ class CalcController {
 
     constructor() {
 
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector('#display');
         this._dateEl = document.querySelector('#data');
@@ -44,6 +45,107 @@ class CalcController {
         this._dateEl.innerHTML = val;
     }
 
+    addEventListenerAll(el, events, fn) {
+
+        events.split(' ').forEach(ev => {
+            el.addEventListener(ev, fn, false);
+        })
+
+    }
+
+    _execBtn(key) {
+
+        switch (key) {
+            case 'ac':
+                this._clearAll();
+                break;
+
+            case 'ce':
+                this._cancelEntry();
+                break;
+
+            case 'soma':
+
+                break;
+
+            case 'subtracao':
+
+                break;
+
+            case 'multiplicacao':
+
+                break;
+
+            case 'divisao':
+
+                break;
+
+            case 'porcento':
+
+                break;
+
+            case 'igual':
+
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+
+                this._addOperation(parseInt(key));
+                break;
+
+            default:
+                this._setError();
+                break;
+
+
+        }
+
+    }
+
+    _clearAll() {
+        this._operation = [];
+    }
+
+    _cancelEntry() {
+        this._operation.pop();
+    }
+
+    _addOperation(val) {
+        this._operation.push(val);
+        console.table(this._operation);
+    }
+
+    _setError() {
+        this.displayCalc = 'Error';
+    }
+
+    _initButtonsEvents() {
+        const buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index) => {
+
+            this.addEventListenerAll(btn, 'click drag', e => {
+                let textBtn = btn.className.baseVal.replace('btn-', '');
+
+                this._execBtn(textBtn);
+            });
+
+            this.addEventListenerAll(btn, 'mouseup mouseover mousedown', e => {
+                btn.style.cursor = 'pointer';
+            });
+
+        });
+    }
+
     _setDisplayDateTime() {
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: '2-digit',
@@ -53,22 +155,11 @@ class CalcController {
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
-    _initButtonsEvents() {
-        const buttons = document.querySelectorAll("#buttons > g, #parts > g");
-
-        buttons.forEach( (btn, index) => {
-
-            btn.addEventListener('click', e => {
-                console.log(btn.className.baseVal.replace('btn-',''));
-            });
-            
-        });
-    }
-
     _init() {
         this._setDisplayDateTime();
         setInterval(() => {
             this._setDisplayDateTime()
         }, 1000);
+        this._initButtonsEvents();
     }
 }
